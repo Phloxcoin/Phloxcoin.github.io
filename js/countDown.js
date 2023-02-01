e65fa681fc4233f33bf206462c66e0135b0ebb34
phloxcoin <98701143+Phloxcoin@users.noreply.github.com>
2023-02-01 15:25:25 +0330

    Add files via upload

--- js/countDown.js
+++ js/countDown.js
@@ -1,7 +1,7 @@
 // Set the date we're counting down to
 // Set the date we're counting down to January February March April May June July August September October November December
 
-var countDownDate = new Date("March 15, 2023 14:30:00").getTime();
+var countDownDate = new Date("March 04, 2023 14:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -21,13 +21,42 @@ var x = setInterval(function() {
   // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-    
+      
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "token launched";
   }
 }, 1000);
+
+var countDownDate1 = new Date("March 15, 2023 14:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate1 - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days1 = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours1 = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes1 = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds1 = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo1").innerHTML = days1 + "d " + hours1 + "h "
+  + minutes1 + "m " + seconds1 + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo1").innerHTML = "token launched";
+  }
+}, 1000);
+
 
 
 

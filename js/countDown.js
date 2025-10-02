3f5b26ec92fe2d2af3855cb0dfcb70229f1a6135
phloxcoin <98701143+Phloxcoin@users.noreply.github.com>
2025-10-02 11:11:29 +0330

    Update countDown.js

--- js/countDown.js
+++ js/countDown.js
@@ -1,7 +1,7 @@
 // Set the date we're counting down to
 // Set the date we're counting down to January February March April May June July August September October November December
 
-var countDownDate = new Date("December 25, 2025 14:30:00").getTime();
+var countDownDate = new Date("December 25, 2025 15:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -56,6 +56,7 @@ var x = setInterval(function() {
     document.getElementById("demo1").innerHTML = "token launched";
   }
 }, 1000);
+
 
 
 
